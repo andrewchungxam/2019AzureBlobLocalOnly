2d0a977ec07c2062af0ebf7e258207b61d18d766
ac <andrew.chung@xamarin.com>
2019-02-25 10:52:12 -0500

    fixed search adding  blank space before search

--- AzureBlobStorageSampleApp/ViewModels/PhotoListViewModel.cs
+++ AzureBlobStorageSampleApp/ViewModels/PhotoListViewModel.cs
@@ -20,6 +20,10 @@ namespace AzureBlobStorageSampleApp
         ObservableCollection<PhotoModel> _allPhotosList;
         String _searchString;
         List<PhotoModel> unsortedPhotosList;
+                //public Command SearchCommand { get; set; }
+
+        ICommand _searchCommand;
+
 
         #endregion
 
@@ -27,10 +31,13 @@ namespace AzureBlobStorageSampleApp
         public ICommand RefreshCommand => _refreshCommand ??
             (_refreshCommand = new AsyncCommand(ExecuteRefreshCommand, continueOnCapturedContext: false));
 
+        public ICommand SearchCommand => _searchCommand ??
+            (_searchCommand = new AsyncCommand(ExecuteSearchCommand, continueOnCapturedContext: false));
+
         public PhotoListViewModel()
         {
             //SearchCommand = new Command(async () => await ExecuteSearchCommand());
-            SearchCommand = new Command(() => ExecuteSearchCommand());
+            //SearchCommand = new Command(async () => await ExecuteSearchCommand());
 
             }
 
@@ -58,17 +65,18 @@ namespace AzureBlobStorageSampleApp
             set => SetProperty(ref _isBusy, value);
         }
 
-        public Command SearchCommand { get; set; }
 
         #endregion
 
         #region Methods
-        //async Task ExecuteSearchCommand()
-        void ExecuteSearchCommand()
+        //async void ExecuteSearchCommand()
+        async Task ExecuteSearchCommand()
         {   
             if(IsBusy)
                 return;
             IsBusy = true;
+
+            AllPhotosList.Clear();
 
              try
             {
@@ -77,16 +85,21 @@ namespace AzureBlobStorageSampleApp
                     //AllPhotosList = new ObservableCollection<PhotoModel>(unsortedPhotosList.Where(x=>x.Title.Any()));//.Where(x => x.Title.Contains(this.SearchString)));
                     //return;
 
-                    AllPhotosList.Clear();
+
                     foreach (var individualPhotos in unsortedPhotosList)
                     {
                         AllPhotosList.Add(individualPhotos);
                     }
+
+                    this.SearchString = "";
+                    //var halfSecondSpiner = Task.Delay(1000);
+                    //await halfSecondSpiner.ConfigureAwait(false);
                 }
                 else
                 { 
+                    //var halfSecondSpiner = Task.Delay(500);
+                    //await halfSecondSpiner.ConfigureAwait(false);
 
-                    AllPhotosList.Clear();
                     foreach (var individualPhotos in unsortedPhotosList.Where(x=>x.Title.Contains(this.SearchString)))
                     {
                         AllPhotosList.Add(individualPhotos);
@@ -118,6 +131,8 @@ namespace AzureBlobStorageSampleApp
             {
 //                var oneSecondTaskToShowSpinner = Task.Delay(1000);
                 var oneSecondTaskToShowSpinner = Task.Delay(700);
+                await oneSecondTaskToShowSpinner.ConfigureAwait(false);
+
 
 
                 if (this.IsInternetConnectionActive == true) { 
